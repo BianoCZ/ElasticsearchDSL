679b745e7fc9ba484bc5a68448218c6388421660
Tomáš Novotný <tomas.novotny@biano.com>
2024-05-28 15:09:03 +0200

    Fix CI

--- src/Aggregation/Bucketing/Ipv4RangeAggregation.php
+++ src/Aggregation/Bucketing/Ipv4RangeAggregation.php
@@ -20,7 +20,7 @@ class Ipv4RangeAggregation extends AbstractBucketingAggregation
     private array $ranges = [];
 
     /**
-     * @param list<array<string,string>> $ranges
+     * @param list<array<string,string>|string> $ranges
      */
     public function __construct(string $name, ?string $field = null, array $ranges = [])
     {
